ef83c8ccb68b33bddb5146a2a051573777b69241
SeriousSoftware <vdenberg+gitkraken@gmail.com>
2019-07-03 17:08:22 +0200

    intermediate save.

--- build.sbt
+++ build.sbt
@@ -1,20 +1,14 @@
-import com.lihaoyi.workbench.Plugin._
-
-enablePlugins(ScalaJSPlugin)
-
-workbenchSettings
+enablePlugins(ScalaJSPlugin, WorkbenchPlugin)
 
 name := "Example"
 
 version := "0.1-SNAPSHOT"
 
-scalaVersion := "2.11.4"
+scalaVersion := "2.12.8"
 
-libraryDependencies ++= Seq(
-  "org.scala-js" %%% "scalajs-dom" % "0.8.0"
-)
+libraryDependencies ++= Seq("org.scala-js" %%% "scalajs-dom" % "0.9.7")
 
-bootSnippet := "example.ScalaJSExample().main(document.getElementById('canvas'));"
+// bootSnippet := "example.ScalaJSExample().main(document.getElementById('canvas'));"
 
-updateBrowsers <<= updateBrowsers.triggeredBy(fastOptJS in Compile)
+// updateBrowsers <<= updateBrowsers.triggeredBy(fastOptJS in Compile)
 
--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,71 +1,93 @@
 package example
 
-import scala.scalajs.js
-import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
-import org.scalajs.dom.html
-import scala.util.Random
-import scalajs.concurrent.JSExecutionContext.Implicits.queue
-import org.scalajs.dom.ext
-case class Point(x: Int, y: Int){
+import org.scalajs.dom.{ext, html}
+
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+import scala.scalajs.js
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+
+case class Point(x: Int, y: Int) {
   def +(p: Point) = Point(x + p.x, y + p.y)
+
   def /(d: Int) = Point(x / d, y / d)
 }
 
-@JSExport
+@JSExportTopLevel("ScalaJSExample")
 object ScalaJSExample {
+  // OpenWeatherMap endpoint details
+  def weatherBaseURL = {
+    def openWeatherMapHost = "openweathermap.org"
+
+    def openWeatherMapAPI = "https://api.".concat(openWeatherMapHost)
+
+    openWeatherMapAPI.concat("/data/2.5/find")
+  }
+
+
   @JSExport
   def main(canvas: html.Canvas): Unit = {
-    val ctx = canvas.getContext("2d")
-                    .asInstanceOf[dom.CanvasRenderingContext2D]
+    val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
 
-    canvas.width = dom.innerWidth
-    canvas.height = dom.innerHeight
+    canvas.width = dom.window.innerWidth.toInt
+    canvas.height = dom.window.innerHeight.toInt
 
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, 10000, 20000)
-    val prefixes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    val allPrefixes = for{
+
+    val prefixes = 'A' to 'Z'
+    val allPrefixes = for {
       a <- prefixes
       b <- prefixes
       c <- prefixes
-    } yield s"$a$b$c"
+    } yield s"$b$a$c"
     var i = 0
 
     def run(prefix: String) = {
+      val owmQueryParams = scala.collection.mutable.Map[String, String](
+        "type" -> "like"
+        , "mode" -> "json"
+        //,"apikey" -> "<Paste your own API Key value here>"
+        , "apikey" -> "9ff16c79edd6ad12396c22ed8a7996ec"
+      )
+
+      owmQueryParams += ("q" -> prefix)
+
+      val queryStr = owmQueryParams.map { case (k, v) => s"$k=$v" }.mkString("?", "&", "")
+
       i += 1
-      println(i + "\t" +  allPrefixes.length)
-      val fut = ext.Ajax.get(s"http://api.openweathermap.org/data/2.5/find?q=$prefix&type=like&mode=json")
-      fut.foreach{ xhr =>
-//        println(xhr.responseText)
-//        println(prefix + "\t" + xhr.responseText.length)
+
+      val fut = ext.Ajax.get(weatherBaseURL.concat(queryStr))
+      fut.foreach { xhr =>
+        // println(prefix + "\t" + xhr.responseText.length)
         val parsed = js.JSON.parse(xhr.responseText)
-//        dom.console.log(parsed)
-        parsed.list.map{ el: js.Dynamic =>
+        //        dom.console.log(parsed)
+        parsed.list.map { el: js.Dynamic =>
           val x = el.coord.lon.asInstanceOf[Double]
           val y = el.coord.lat.asInstanceOf[Double]
           val t = el.main.temp.asInstanceOf[Double] // 250 -> 350
-          val screenX =  (x / 180 + 1) / 2 * canvas.width
+
+          println(s"$queryStr, Reponse $x $y $t")
+
+
+          val screenX = (x / 180 + 1) / 2 * canvas.width
           val screenY = canvas.height - (y / 90 + 1) / 2 * canvas.height
 
           val tScaled = ((t - 260) / 50 * 255).toInt // 0 - 255
-          val (r, g, b) = color(tScaled)
-          println(t)
+        val (r, g, b) = color(tScaled)
           ctx.fillStyle = s"rgb($r, $g, $b)"
           ctx.fillRect(screenX - 2, screenY - 2, 4, 4)
         }
 
       }
     }
+
     def color(tScaled: Int) = { // 0 -> 255
       val r = math.max(tScaled - 128, 0) * 2
       val g = (128 - math.abs(tScaled - 128)) * 2
       val b = math.max(128 - tScaled, 0) * 2
       (r, g, b)
     }
-//    println(color(0))
-//    println(color(128))
-//    println(color(255))
-    dom.setInterval(() => run(allPrefixes(i)), 10)
+    dom.window.setInterval(() => run(allPrefixes(i)), 2000)
   }
 }
